d7d22098545f2c789aabea6e6ef4b85e6c29761e
Owais Ali Khan <owaisalikhan2003@gmail.com>
2019-07-17 19:52:29 +0500

    Added dict_histogram.py

---
+++ python/dict_histogram.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def histogram(x):
+    d = dict()
+    for i in sorted(x):
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+    return d
+def print_hist(x):
+    for i in x:
+        print(i,x[i])
+h = histogram("brontosaurus")
+print(h)
+print(h.get("a",0))
+print_hist(h)
+
+
+
--- python/words.py
+++ python/words.py
@@ -2,4 +2,6 @@
 
 words = ['cat', 'window', 'defenestrate']
 for w in words:
-    print(w,len(w))
+    print(w, len(w))
+for i in range(len(words)):
+    print(i, words[i])
